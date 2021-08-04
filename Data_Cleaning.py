638c68096f21dc9b421e0177256c752eaa5b6127
GianmarcoMulaz <55657424+GianmarcoMU@users.noreply.github.com>
2021-08-04 18:26:22 +0200

    Update Data_Cleaning.py

--- Data_Cleaning.py
+++ Data_Cleaning.py
@@ -1,31 +1,20 @@
-### First rows are just basic commands to learn Python
-
 #Import some relevant libraries
-
-import math as mt 
-import datetime as dt
 import pandas as pd
-
-#Define just some variables in Python
-
-a = 5
-b = 'prova'
-c = 3/4
-
-list1 = [1, 2, 3, 4, 5]
-list2 = [0, 1, 0, 1, 0]
-list3 = [2, 2, 1, 1, 5]
-
-list4 = [list1, list2, list3]
-print(list4)
-
-#Some exercises with the libraries
-
-data = pd.DataFrame({'Lista 1': list1, 'Lista 2': list2, 'Lista 3': list3})
-print(data)
+import numpy as np
 
 #Import and read a CSV file
 #We add the 'r' before the path specification to solve an error that occured otherwise
 file_path = r"C:\Users\mulaz\Desktop\GIANMARCO\BOCCONI\MSc - ESS\A.Y. 2020-21\SPRING SEMESTER\FINAL THESIS\DATA\TRTH_L2\Mulazzani\split\Market Depth NYSE_chunk1.csv"
-chunck1 = pd.read_csv(file_path)
-print(chunck1.head())
+df = pd.read_csv(file_path)
+#print(chunck1['Date-Time'].head())
+
+# The idea of the first step is to separate date and time in two different columns
+# then we need to normalise the hour and so subtract 4, then delete the useless columns
+# this should be done for all csv file and all rows, thus we need a for loop
+
+# This code is for extracting Date-Time column, select the hour and subtract GMT offset
+# It does not work
+slice = df.iloc[:,2]
+print(slice[11:13])
+
+#print(int(slice[11:13])-4)
