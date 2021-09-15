6c4c3bd5faddd79dbe7684f242ef7e97cd313744
GianmarcoMulaz <55657424+GianmarcoMU@users.noreply.github.com>
2021-09-15 13:47:26 +0200

    Add files via upload

--- Data Cleaning.py
+++ Data Cleaning.py
@@ -13,44 +13,79 @@
 import pandas as pd
 import numpy as np
 import datetime as dt
+from statistics import stdev, mean
+from pathlib import Path
 
 # Import data and create a dataframe
-df = pd.read_csv('prova.csv')
+
+folder = r"C:\Users\mulaz\Desktop\DATA"
+
+for file in Path(folder).glob('*.csv'):
+    df = pd.read_csv(file)
+
+    format = "%H:%M:%S"
+
+    opening = "13:30:00"
+    closing = "16:00:00"
+    check_o = dt.datetime.strptime(opening, format).time()
+    check_c = dt.datetime.strptime(closing, format).time()
+
+    df['Date'] = df['Date-Time'].str[:10]
+    df['Time'] = df['Date-Time'].str[11:-1]
+    df['Time_Adj'] = ""
+    df['Check'] = ""
+
+    for i in range(0,20):
+        df.iloc[i, -2] = dt.datetime.strptime(df.iloc[i, -3][0:8], format).time()
+        df.iloc[i, -1] = (df.iloc[i,-2] < check_o) or (df.iloc[i,-2] > check_c)
+
+    df.drop(df.loc[df['Check']==True].index, inplace=True)   
+    df.reset_index(drop=True, inplace=True)
+
+    print(df.head(10))
+
+
 #print(df.head(5)) # Print the first five rows
 #print(df.columns) # Print the name of the columns
 #print(df.iloc[1, 2]) # Print element in row 2 and column 3
 
 # Create a for loop to retrieve all the rows for a specific column
-for index, row in df.iterrows():
-    print(row[2])
+#for index, row in df.iterrows():
+ #   print(row[2])
 
 #print(type(df.iloc[1, 2])) # Here we check that the content of Date-Time is a string
 
 # Here we create two new columns to separate Date and Time
 
-format = "%H:%M:%S"
 
-opening = "13:10:00"
-closing = "16:00:00"
-check_o = dt.datetime.strptime(opening, format).time()
-check_c = dt.datetime.strptime(closing, format).time()
-
-df['Date'] = df['Date-Time'].str[:10]
-df['Time'] = df['Date-Time'].str[11:-1]
-df['Time_Adj'] = ""
-df['Check'] = ""
-
-for i in range(0,10):
-    df.iloc[i, -2] = dt.datetime.strptime(df.iloc[i, -3][0:8], format).time()
-    df.iloc[i, -1] = (df.iloc[i,-2]<check_o) or (df.iloc[i,-2]>check_c)
-
-df.drop(df.loc[df['Check']==True].index, inplace=True)   
-df.reset_index(drop=True, inplace=True)
-
-print(df.head(5))
 
 ## Now that we have cleaned the dataset, we need to normalise it. In order to do so, we need to
 ## compute mean and std.dev for both prices and volumes using the previous five days at any given point.
+
+## An idea could be to initialize empty dictionaries and then populate them with the obs of previous days. This could be done by using while loops
+## check the agenda for more details. 
+
+five_previous_prices = list()
+five_previous_volumes = list()
+
+z = 5
+h = 5
+
+while z < 45:
+    for x in range(1, 6, 1):
+        five_previous_prices.append(df.iloc[5-x, z])
+        five_previous_volumes.append(df.iloc[5-x, z+1])
+    z += 2
+
+
+while h < 45:
+    df.iloc[5, h] = (df.iloc[5, h] - mean(five_previous_prices))/stdev(five_previous_prices)
+    h += 2
+
+print(df.iloc[5,5])
+
+
+
 
 
 
